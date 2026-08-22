ffb967287f446be5bde81998b88eed4c6443b1a3
IKazarbekov <ikazarbekov@bk.ru>
2026-08-22 13:46:48 +0500

    version 0.0.17 - refactoring main window pyqt

--- apps/PC/main.py
+++ apps/PC/main.py
@@ -1,12 +1,15 @@
 import sys, functools
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QVBoxLayout, QLabel, QStackedLayout, QFormLayout, QLineEdit, QSpinBox, QComboBox, QTableWidget
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QVBoxLayout, QLabel, QStackedLayout, \
+    QFormLayout, QLineEdit, QSpinBox, QComboBox, QTableWidget, QHBoxLayout
 from py_model.session import Session
 from py_model.student import Student
 from py_model.dto.student import Student as DTOStudent
 from apps.PC.utils import database, dto_mapper
 import game_launcher
 from datetime import datetime
-
 
 class StartWindow(QMainWindow):
     def __init__(self):
@@ -15,24 +18,20 @@ class StartWindow(QMainWindow):
         self.dto_students = dto_mapper.get_all_dto_students()
         # UI
         super().__init__()
-        self.setWindowTitle("Benefit Harm Launcher 0.0.1")
-        self.setFixedSize(800, 400)
+        self.setWindowTitle("Benefit Harm Launcher")
+        self.setWindowIcon(QIcon("apps/PC/icon.png"))
+        self.setMinimumSize(700, 350)
         self.initUI()
 
     def initUI(self):
         """"""
         # region Main menu
         label = QLabel("Benefit Harm Launcher")
-        button_student = QPushButton("Ученик", self)
-        button_teacher = QPushButton("Учитель", self)
-
-        button_student.clicked.connect(self.session_start_action)
-        button_student.setEnabled(False)
-        button_teacher.clicked.connect(lambda : self.stacked.setCurrentIndex(1))
+        button_teacher = QPushButton("Начать", self)
+        button_teacher.clicked.connect(lambda : self.stacked.setCurrentWidget(self.screen_teacher))
 
         main_vbox = QVBoxLayout(self)
         main_vbox.addWidget(label)
-        main_vbox.addWidget(button_student)
         main_vbox.addWidget(button_teacher)
         # endregion
 
@@ -40,11 +39,13 @@ class StartWindow(QMainWindow):
         teacher_label = QLabel("Кабинет учителя")
         teacher_label_table = QLabel("Ученики:")
         self.teacher_table = QTableWidget()
-        self.teacher_table.setColumnCount(4)
-        self.teacher_table.setHorizontalHeaderLabels(["Имя", "Класс",  "Последний сеанс", "Сеанс"])
+        self.teacher_table.setColumnCount(5)
+        self.teacher_table.setHorizontalHeaderLabels(["Имя", "Класс",  "Последний сеанс", "Сеанс", "Данные"])
         self.teacher_table.setColumnWidth(2, 180)
+        self.teacher_table.setColumnWidth(3, 140)
+        self.teacher_table.setColumnWidth(4, 140)
         teacher_button_add_student = QPushButton("Добавить ученика", self)
-        teacher_button_add_student.clicked.connect(lambda : self.stacked.setCurrentIndex(2))
+        teacher_button_add_student.clicked.connect(lambda : self.stacked.setCurrentWidget(self.screen_add_student))
 
         teacher_vbox = QVBoxLayout()
         teacher_vbox.addWidget(teacher_label)
@@ -63,6 +64,9 @@ class StartWindow(QMainWindow):
         self.addendum_student_class = QLineEdit()
 
         addendum_student_label = QLabel("Добавить ученика")
+        addendum_student_back = QPushButton("Назад")
+        addendum_student_back.setFixedWidth(100)
+        addendum_student_back.clicked.connect(lambda: self.stacked.setCurrentWidget(self.screen_teacher))
         addendum_student_form = QFormLayout()
         addendum_student_form.addRow("ФИО", self.addendum_student_name)
         addendum_student_form.addRow("Возраст", self.addendum_student_sp)
@@ -73,22 +77,39 @@ class StartWindow(QMainWindow):
 
         addendum_student_vbox = QVBoxLayout()
         addendum_student_vbox.addWidget(addendum_student_label)
+        addendum_student_vbox.addWidget(addendum_student_back)
         addendum_student_vbox.addLayout(addendum_student_form)
         addendum_student_vbox.addWidget(addendum_student_button)
         # endregion
 
+        # region Student data
+        student_data_label = QLabel("Данные студента")
+        student_data_button_back = QPushButton("Назад")
+        student_data_button_back.setFixedWidth(150)
+        student_data_button_back.clicked.connect(lambda: self.stacked.setCurrentWidget(self.screen_teacher))
+        self.student_data_form = QFormLayout()
+
+        student_data_vbox = QVBoxLayout()
+        student_data_vbox.addWidget(student_data_label)
+        student_data_vbox.addWidget(student_data_button_back)
+        student_data_vbox.addLayout(self.student_data_form)
+        # endregion
+
         # region Boxes
-        screen_main = QWidget()
-        screen_main.setLayout(main_vbox)
-        screen_teacher = QWidget()
-        screen_teacher.setLayout(teacher_vbox)
-        screen_add_student = QWidget()
-        screen_add_student.setLayout(addendum_student_vbox)
+        self.screen_main = QWidget()
+        self.screen_main.setLayout(main_vbox)
+        self.screen_teacher = QWidget()
+        self.screen_teacher.setLayout(teacher_vbox)
+        self.screen_add_student = QWidget()
+        self.screen_add_student.setLayout(addendum_student_vbox)
+        self.screen_student_data = QWidget()
+        self.screen_student_data.setLayout(student_data_vbox)
 
         self.stacked = QStackedLayout()
-        self.stacked.addWidget(screen_main)
-        self.stacked.addWidget(screen_teacher)
-        self.stacked.addWidget(screen_add_student)
+        self.stacked.addWidget(self.screen_main)
+        self.stacked.addWidget(self.screen_teacher)
+        self.stacked.addWidget(self.screen_add_student)
+        self.stacked.addWidget(self.screen_student_data)
 
         central_widget = QWidget()
         central_widget.setLayout(self.stacked)
@@ -111,8 +132,10 @@ class StartWindow(QMainWindow):
         self.teacher_table.setRowCount(len(students))
         for i, student in enumerate(students):
 
-            button = QPushButton("начать")
-            button.clicked.connect(functools.partial(begin_session, student.db_id))
+            button_session = QPushButton("Начать")
+            button_session.clicked.connect(functools.partial(begin_session, student.db_id))
+            button_data = QPushButton("Посмотреть")
+            button_data.clicked.connect(lambda: self.stacked.setCurrentWidget(self.screen_student_data))
             str_last_time = "Не было"
             if student.str_last_time_session:
                 str_last_time = student.str_last_time_session
@@ -121,7 +144,8 @@ class StartWindow(QMainWindow):
             self.teacher_table.setCellWidget(i, 0, QLabel(student.name))
             self.teacher_table.setCellWidget(i, 1, QLabel(student.class_))
             self.teacher_table.setCellWidget(i, 2, label_last_session)
-            self.teacher_table.setCellWidget(i, 3, button)
+            self.teacher_table.setCellWidget(i, 3, button_session)
+            self.teacher_table.setCellWidget(i, 4, button_data)
 
     def session_start_action(self, student: Student):
         """
@@ -169,11 +193,18 @@ class StartWindow(QMainWindow):
             str_last_time_session="не было"
         )
         self.dto_students.append(new_dto_student)
-        self.stacked.setCurrentIndex(1)
+        self.stacked.setCurrentWidget(self.screen_teacher)
         self.update_table_students()
+
+    def watch_student_data(self):
+        pass
 
 # run
 if __name__ == '__main__':
+    import ctypes
+    myappid = 'mycompany.myproduct.myapp'  # Вы можете использовать любое уникальное имя
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+
     app = QApplication(sys.argv)
     window = StartWindow()
     window.show()
